45137b328c30cf51aea0043029452f9d303e885a
rakibulhasanutsho <162662167+rakibulhasanutsho@users.noreply.github.com>
2026-04-03 14:35:07 +0600

    add code for dynamically count cart price and show display

--- src/components/Carts.jsx
+++ src/components/Carts.jsx
@@ -1,8 +1,9 @@
 import { FaShoppingBasket } from "react-icons/fa"
 export default  function Carts({carts}) {
+  const totalPrice = carts.reduce((sum, product) => sum + product.price, 0)
   console.log(carts)
     return (
-      <div className="w-[1140px] mx-auto border border-gray-300 p-15 flex flex-col gap-4">
+      <div className="w-[1140px] mx-auto border border-gray-300 p-15 flex flex-col gap-4 rounded">
         <h3 className="text-3xl font-bold ">Your Cart</h3>
         { 
         
@@ -30,7 +31,7 @@ export default  function Carts({carts}) {
         <div className="space-y-4">
           <div className="flex justify-between">
             <p>Total :</p>
-            <h3 className="text-2xl font-bold text-[#101727] ">$300</h3>
+            <h3 className="text-2xl font-bold text-[#101727] ">${totalPrice}</h3>
           </div>
           <button className="bg-gradient-to-r from-[#4F39F6] to-[#9514FA] w-full p-3 cursor-pointer rounded-4xl text-[16px] text-white text-semibold">Proceed to Checkout</button>
         </div>
